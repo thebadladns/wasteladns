1279eaf0335a31490f9b43b22a251509442ce815
Carlos <carloscatalanfdez@gmail.com>
2018-11-03 13:09:01 -0400

    Use CMAKE >3.6 features only if they are available, fixed bad backbuffer settings preventing directx device from being created, extracted printf into helper and adjusted tabbing to better handle de-initializations

--- c++/src/TestPlatformAPI/main.cpp
+++ c++/src/TestPlatformAPI/main.cpp
@@ -66,7 +66,7 @@ int main(int argc, char** argv) {
 
     int returnValue = 1;
     returnValue = Platform::GLFW::main<Game::Instance>(argc, argv);
-    
+
     return returnValue;
 }
 #endif
@@ -74,212 +74,236 @@ int main(int argc, char** argv) {
 #if PLATFORM_DIRECTX9
 
 #include <stdio.h>
-#include <windows.h>
-#undef min
-#undef max
-#undef DELETE
+#define _X86_
+#if defined _M_X64 || defined _M_AMD64
+#undef _X86_
+#define _AMD64_
+#endif
+#define NOMINMAX
+#include <windef.h>
 #include <d3d9.h>
+#undef near
+#undef far
 
 struct Time {
-	struct Config {
-		f64 targetFramerate;
-		f64 maxFrameLength;
-	};
+    struct Config {
+        f64 targetFramerate;
+        f64 maxFrameLength;
+    };
 
-	Config config;
-	f64 lastFrame;
-	f64 lastFrameDelta;
-	f64 nextFrame;
-	s64 frameCount;
-	bool paused;
+    Config config;
+    f64 lastFrame;
+    f64 lastFrameDelta;
+    f64 nextFrame;
+    s64 frameCount;
+    bool paused;
+    float far;
 };
 struct Game {
-	Time time;
+    Time time;
 };
 
-void start(Game& game, Platform::GameConfig& config, const Platform::State& platform) {
-	game.time = {};
-	game.time.config.maxFrameLength = 0.1;
-	game.time.config.targetFramerate = 1.0 / 60.0;
-	game.time.lastFrame = platform.time.now;
+namespace Platform {
 
-	config = {};
-	config.nextFrame = platform.time.now;
-	config.requestFlags = Platform::RequestFlags::PollKeyboard;
+    void printf(const char* format, ...) {
+        char buffer[256];
+        va_list va;
+        va_start(va, format);
+        vsprintf_s(buffer, format, va);
+        va_end(va);
+        OutputDebugString(buffer);
+    }
+
+}
+namespace OS = Platform;
+
+void start(Game& game, Platform::GameConfig& config, const Platform::State& platform) {
+    game.time = {};
+    game.time.config.maxFrameLength = 0.1;
+    game.time.config.targetFramerate = 1.0 / 60.0;
+    game.time.lastFrame = platform.time.now;
+
+    config = {};
+    config.nextFrame = platform.time.now;
+    config.requestFlags = Platform::RequestFlags::PollKeyboard;
 
 };
 void update(Game& game, Platform::GameConfig& config, const Platform::State& platform) {
 
-	f64 raw_dt = platform.time.now - game.time.lastFrame;
-	game.time.lastFrameDelta = Math::min(raw_dt, game.time.config.maxFrameLength);
-	game.time.lastFrame = platform.time.now;
-	config.nextFrame = platform.time.now + game.time.config.targetFramerate;
-	
-	char str[256];
-	sprintf_s(str, "%6f % 6f\n", raw_dt, platform.time.running);
+    f64 raw_dt = platform.time.now - game.time.lastFrame;
+    game.time.lastFrameDelta = Math::min(raw_dt, game.time.config.maxFrameLength);
+    game.time.lastFrame = platform.time.now;
+    config.nextFrame = platform.time.now + game.time.config.targetFramerate;
 
-	OutputDebugString(str);
+    OS::printf("%6f %6f\n", raw_dt, platform.time.running);
 };
 
 namespace Platform {
-	void loadLaunchConfig(Platform::WindowConfig& config) {
-		// hardcoded for now
-		config.window_width = 640;
-		config.window_height = 480;
-		config.fullscreen = false;
-		config.title = "Platform API Test";
-	};
-	template<>
-	void start<Game>(Game& game, Platform::GameConfig& config, const Platform::State& platform) {
-		::start(game, config, platform);
-	};
-	template<>
-	void update<Game>(Game& game, Platform::GameConfig& config, const Platform::State& platform) {
-		::update(game, config, platform);
-	};
+    void loadLaunchConfig(Platform::WindowConfig& config) {
+        // hardcoded for now
+        config.window_width = 640;
+        config.window_height = 480;
+        config.fullscreen = false;
+        config.title = "Platform API Test";
+    };
+    template<>
+    void start<Game>(Game& game, Platform::GameConfig& config, const Platform::State& platform) {
+        ::start(game, config, platform);
+    };
+    template<>
+    void update<Game>(Game& game, Platform::GameConfig& config, const Platform::State& platform) {
+        ::update(game, config, platform);
+    };
 };
 
 namespace Platform {
-namespace DIRECTX9 {
+    namespace DIRECTX9 {
 
-	LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-		switch (message) {
-		case WM_DESTROY: {
-			PostQuitMessage(0);
-			return 0;
-		}
-		case WM_KEYDOWN:
-		case WM_SYSKEYDOWN:
-		case WM_KEYUP:
-		case WM_SYSKEYUP:
-			if (message == WM_KEYUP && wParam == VK_ESCAPE) {
-				PostQuitMessage(0);
-				return 0;
-			}
-			break;
-		}
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
+        LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+            switch (message) {
+            case WM_DESTROY: {
+                PostQuitMessage(0);
+                return 0;
+            }
+            case WM_KEYDOWN:
+            case WM_SYSKEYDOWN:
+            case WM_KEYUP:
+            case WM_SYSKEYUP:
+                if (message == WM_KEYUP && wParam == VK_ESCAPE) {
+                    PostQuitMessage(0);
+                    return 0;
+                }
+                break;
+            }
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
 
-	template <typename _GameData>
-	int main(HINSTANCE hInstance,
-		HINSTANCE hPrevInstance,
-		LPSTR lpCmdLine,
-		int nCmdShow) {
+        template <typename _GameData>
+        int main(
+              HINSTANCE hInstance
+            , HINSTANCE hPrevInstance
+            , LPSTR lpCmdLine
+            , int nCmdShow
+        ) {
+            (void)lpCmdLine;
+            (void)hPrevInstance;
 
-		(void)lpCmdLine;
-		(void)hPrevInstance;
+            HWND hWnd;
+            Platform::State platform = {};
+            {
+                WNDCLASSEX wc;
+                ZeroMemory(&wc, sizeof(WNDCLASSEX));
+                wc.cbSize = sizeof(WNDCLASSEX);
+                // Redraw on horizontal or vertical resize
+                wc.style = CS_HREDRAW | CS_VREDRAW;
+                wc.lpfnWndProc = WindowProc;
+                wc.hInstance = hInstance;
+                wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+                wc.lpszClassName = "WindowClass";
+                RegisterClassEx(&wc);
 
-		HWND hWnd;
-		Platform::State platform = {};
+                Platform::WindowConfig windowConfig;
+                loadLaunchConfig(windowConfig);
 
-		{
-			WNDCLASSEX wc;
-			ZeroMemory(&wc, sizeof(WNDCLASSEX));
-			wc.cbSize = sizeof(WNDCLASSEX);
-			// Redraw on horizontal or vertical resize
-			wc.style = CS_HREDRAW | CS_VREDRAW;
-			wc.lpfnWndProc = WindowProc;
-			wc.hInstance = hInstance;
-			wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-			wc.lpszClassName = "WindowClass";
-			RegisterClassEx(&wc);
+                hWnd = CreateWindowEx(
+                    NULL
+                    , "WindowClass"
+                    , windowConfig.title
+                    , WS_OVERLAPPEDWINDOW
+                    , 0, 0, windowConfig.window_width, windowConfig.window_height
+                    , NULL /*parent*/
+                    , NULL /*menu*/
+                    , hInstance
+                    , NULL
+                );
+                if (hWnd == NULL) {
+                    return 0;
+                }
+                ShowWindow(hWnd, nCmdShow);
 
-			Platform::WindowConfig windowConfig;
-			loadLaunchConfig(windowConfig);
+                platform.screen.width = windowConfig.window_width;
+                platform.screen.height = windowConfig.window_height;
+                platform.screen.desiredRatio = platform.screen.width / (f32)platform.screen.height;
+                platform.screen.fullscreen = windowConfig.fullscreen;
+            }
 
-			hWnd = CreateWindowEx(
-				  NULL
-				, "WindowClass"
-				, windowConfig.title
-				, WS_OVERLAPPEDWINDOW
-				, 0, 0, windowConfig.window_width, windowConfig.window_height
-				, NULL /*parent*/
-				, NULL /*menu*/
-				, hInstance
-				, NULL
-			);
-			if (hWnd == NULL) {
-				return 0;
-			}
-			ShowWindow(hWnd, nCmdShow);
+            u64 frequency;
+            if (!QueryPerformanceFrequency((LARGE_INTEGER*)&frequency)) {
+                return 0;
+            }
 
-			platform.screen.width = windowConfig.window_width;
-			platform.screen.height = windowConfig.window_height;
-			platform.screen.desiredRatio = platform.screen.width / (f32)platform.screen.height;
-			platform.screen.fullscreen = windowConfig.fullscreen;
-		}
+            LPDIRECT3D9 d3d = Direct3DCreate9(D3D_SDK_VERSION);
+            if (d3d == NULL) {
+                return 0;
+            }
 
-		u64 frequency;
-		if (!QueryPerformanceFrequency((LARGE_INTEGER*)&frequency)) {
-			return 0;
-		}
+            LPDIRECT3DDEVICE9 d3ddev;
+            D3DPRESENT_PARAMETERS d3dpp;
+            ZeroMemory(&d3dpp, sizeof(d3dpp));
+            d3dpp.Windowed = TRUE; // TODO: support fullscreen
+            d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+            d3dpp.hDeviceWindow = hWnd;
+            d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
+            d3dpp.BackBufferWidth = platform.screen.width;
+            d3dpp.BackBufferHeight = platform.screen.height;
 
-		LPDIRECT3D9 d3d;
-		LPDIRECT3DDEVICE9 d3ddev;
-		{
-			d3d = Direct3DCreate9(D3D_SDK_VERSION);
-			D3DPRESENT_PARAMETERS d3dpp;
-			ZeroMemory(&d3dpp, sizeof(d3dpp));
-			d3dpp.Windowed = true; // TODO: support fullscreen
-			d3dpp.hDeviceWindow = hWnd;
-			d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
-			d3dpp.BackBufferWidth = platform.screen.width;
-			d3dpp.BackBufferHeight = platform.screen.height;
+            d3d->CreateDevice(
+                D3DADAPTER_DEFAULT
+                , D3DDEVTYPE_HAL
+                , hWnd
+                , D3DCREATE_SOFTWARE_VERTEXPROCESSING
+                , &d3dpp
+                , &d3ddev
+            );
 
-			d3d->CreateDevice(
-				  D3DADAPTER_DEFAULT
-				, D3DDEVTYPE_HAL
-				, hWnd
-				, D3DCREATE_SOFTWARE_VERTEXPROCESSING
-				, &d3dpp
-				, &d3ddev
-			);
-		}
+            if (d3ddev != NULL) {
+                u64 start;
+                QueryPerformanceCounter((LARGE_INTEGER*)&start);
 
-		u64 start;
-		QueryPerformanceCounter((LARGE_INTEGER*)&start);
+                platform.time.running = 0.0;
+                platform.time.now = platform.time.start = start / (f64)frequency;
 
-		platform.time.running = 0.0;
-		platform.time.now = platform.time.start = start / (f64) frequency;
+                _GameData game;
+                Platform::GameConfig config;
+                Platform::start<_GameData>(game, config, platform);
 
-		_GameData game;
-		Platform::GameConfig config;
-		Platform::start<_GameData>(game, config, platform);
+                MSG msg;
 
-		MSG msg;
+                do {
+                    while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+                        TranslateMessage(&msg);
+                        DispatchMessage(&msg);
+                    }
 
-		do {
-			while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
+                    if (platform.time.now >= config.nextFrame) {
 
-			if (platform.time.now >= config.nextFrame) {
+                        Platform::update<_GameData>(game, config, platform);
+                    }
 
-				Platform::update<_GameData>(game, config, platform);
-			}
+                    u64 now;
+                    QueryPerformanceCounter((LARGE_INTEGER*)&now);
+                    platform.time.now = now / (f64)frequency;
+                    platform.time.running = platform.time.now - platform.time.start;
+                } while (msg.message != WM_QUIT);
 
-			u64 now;
-			QueryPerformanceCounter((LARGE_INTEGER*)&now);
-			platform.time.now = now / (f64) frequency;
-			platform.time.running = platform.time.now - platform.time.start;
-		} while (msg.message != WM_QUIT);
+                d3ddev->Release();
+            }
+            d3d->Release();
 
-		return 1;
-	}
-}
+            return 1;
+        }
+    }
 }
 
 int WINAPI WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow) {
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow) {
 
-	int returnValue = 1;
-	returnValue = Platform::DIRECTX9::main<Game>(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+    int returnValue = 1;
+    returnValue = Platform::DIRECTX9::main<Game>(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 
-	return returnValue;
+    return returnValue;
 
 }
 
