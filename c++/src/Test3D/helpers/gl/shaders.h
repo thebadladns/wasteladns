b0013e9688a8469c9004caaa14086a1d5fefc25f
Carlos Jambrina <carloscatalanfdez@gmail.com>
2023-12-18 21:19:27 -0500

    On mac, GL seems to want the pixel shader's input to have the same name as the vertex shader's output; so we'll manually fix it for now

--- c++/src/Test3D/helpers/gl/shaders.h
+++ c++/src/Test3D/helpers/gl/shaders.h
@@ -31,12 +31,12 @@ const char* defaultPixelShaderStr = R"(
 #version 330
 #extension GL_ARB_separate_shader_objects : require
 
-layout(location = 0) in vec4 in_var_COLOR;
+layout(location = 0) in vec4 out_var_COLOR;
 layout(location = 0) out vec4 out_var_SV_TARGET;
 
 void main()
 {
-    out_var_SV_TARGET = in_var_COLOR;
+    out_var_SV_TARGET = out_var_COLOR;
 }
 
 )";
