5b2ef379af2eb8145188469ff0e42f7b63c50ce8
Carlos <carloscatalanfdez@gmail.com>
2025-01-30 13:40:54 -0500

    Improve Windows' build.bat so that it correctly generates a PDB file that Visual Studio can use when attached

--- c++_2024-/build.bat
+++ c++_2024-/build.bat
@@ -20,35 +20,41 @@ if "%dx11%"=="1" set gl33=0
 if "%gl33%"=="1" set dx11=0
 
 :: figure out compile args
-set cl_debug=call cl /Od /Ob1
+:: /Od=no optimization, /Ob1=respect __inline, /Zi=generate PDB
+set cl_debug=call cl /Od /Ob1 /Zi
+:: /O2=speed optimization, /DNDEBUG=define non-debug macro to match CMake
 set cl_release=call cl /O2 /DNDEBUG
 if "%dx11%"=="1" (
+    set outname=app-dx11
     set compileoutdir=.\app-dx11.dir
-    set flags=/D__DX11=1
+    set flags=/D__DX11=1 
     set libs=user32.lib
 )
 if "%gl33%"=="1" (
+    set outname=app-gl33
     set compileoutdir=.\app-gl33.dir
     set flags=/D__GL33=1
     set libs=user32.lib gdi32.lib
 )
 if "%debug%"=="1" (
     set outdir=.\bin\Debug
-    set objectdir=%compileoutdir%\Debug\
+    ::set objectdir=%compileoutdir%\Debug\
     set compile=%cl_debug%
+    set linkflags=/link /incremental:no /PDB:%outdir%\%outname%.pdb
 )
 if "%release%"=="1" (
     set outdir=.\bin\Release\
-    set objectdir=%compileoutdir%\Release\
+    ::set objectdir=%compileoutdir%\Release\
     set compile=%cl_release%
+    set linkflags=/link /incremental:no
 )
 
 :: create dirs
 if not exist %outdir% mkdir %outdir%
-if not exist %objectdir% mkdir %objectdir%
+::if not exist %objectdir% mkdir %objectdir%
 
 :: build
-%compile% /Fe%outdir%\app-dx11.exe /Fo%objectdir% src\main.cpp %libs% /D__WIN64=1 %flags%
+%compile% /nologo /Fe%outdir%\%outname%.exe src\main.cpp %libs% /D__WIN64=1 %flags% %linkflags%
 
 :: post-build step (/E for recursive, /Y for overwrite without prompting)
 if "%assets%"=="1" xcopy .\assets\ %outdir%\assets\ /E /Y
